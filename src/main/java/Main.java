f42e8eaa10002e799ba188c48118394d2ddd8c30
NastiaFasova <59768948+NastiaFasova@users.noreply.github.com>
2020-05-20 11:14:58 +0300

    Created model Movie, MovieDao  Hibernate based implementation and Mov… (#1)
    * Created model Movie, MovieDao  Hibernate based implementation and MovieService
    
    * Changed the constructor of exception, renamed the movieId
    
    Co-authored-by: NastiaFasova <fasovaaa@fmail.com>

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import cinema.lib.Injector;
+import cinema.model.Movie;
+import cinema.service.MovieService;
+
+public class Main {
+    private static Injector injector = Injector.getInstance("cinema");
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Fast and Furious");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(movie);
+
+        movieService.getAll().forEach(System.out::println);
+    }
+}
