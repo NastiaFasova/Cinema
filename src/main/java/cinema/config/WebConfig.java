00ef61fd81e98bb94631617d5eb2279cad95a74b
NastiaFasova <fasovaaa@fmail.com>
2020-06-29 23:56:09 +0300

    Removed unnecessary imports

--- src/main/java/cinema/config/WebConfig.java
+++ src/main/java/cinema/config/WebConfig.java
@@ -3,8 +3,6 @@ package cinema.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
