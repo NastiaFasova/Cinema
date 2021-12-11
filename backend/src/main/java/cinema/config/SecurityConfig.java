8d06d7a9f74ef95180d6fc43ef5d90f3aadb8ad7
NastiaFasova <fasovaaa@gmail.com>
2021-12-11 13:51:18 +0200

    Added jwts (#16)
    * change project structure
    
    * frontend init
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Added jwts
    
    Co-authored-by: Yuriy Soproniuk <sopronukCC@gmail.com>

--- backend/src/main/java/cinema/config/SecurityConfig.java
+++ backend/src/main/java/cinema/config/SecurityConfig.java
@@ -1,20 +1,31 @@
 package cinema.config;
 
+import cinema.security.JwtCsrfFilter;
+import cinema.security.JwtTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+    @Autowired
+    private JwtTokenRepository jwtTokenRepository;
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -25,6 +36,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                .and()
+                .addFilterAt(new JwtCsrfFilter(jwtTokenRepository, resolver), CsrfFilter.class)
+                .csrf().ignoringAntMatchers("/**")
+                .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/movies/**",
                         "/movie-sessions/**", "/cinema-halls/**")
@@ -32,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/orders/complete",
                         "/shopping-carts/**")
                 .hasRole("USER")
-                .antMatchers(HttpMethod.GET, "/register", "/hello", "/movies",
+                .antMatchers(HttpMethod.GET, "/register", "/login", "/hello", "/movies",
                         "/movie-sessions", "/cinema-halls", "/orders")
                 .hasAnyRole("USER", "ADMIN")
                 .anyRequest().authenticated()
@@ -42,7 +59,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic()
                 .and()
-                .csrf().disable();
+                .httpBasic()
+                .authenticationEntryPoint(((request, response, e) -> resolver.resolveException(request, response, null, e)));
+        ;
     }
 
     @Bean
--- backend/src/main/java/cinema/model/Movie.java
+++ backend/src/main/java/cinema/model/Movie.java
@@ -8,6 +8,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "movie")
+@Data
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
